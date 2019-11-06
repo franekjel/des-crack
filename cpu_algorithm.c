d065e3ea1806468182f742c27272026c1ce6d67c
Franciszek Jełowicki <franek.jelowicki@gmail.com>
2019-11-06 22:47:05 +0100

    Phase 2 without f function

--- cpu_algorithm.c
+++ cpu_algorithm.c
@@ -25,6 +25,19 @@ inline static uint64_t permutate56To48(uint64_t data, uint8_t perm_table[])
     return re;
 }
 
+inline static uint64_t permutate64To64(uint64_t data, uint8_t perm_table[])
+{
+    uint64_t re = 0;
+
+    for (int i = 0; i < 64; i++) {
+        uint64_t v = (data & (1L << perm_table[i]));
+        v >>= perm_table[i];
+        v <<= i;
+        re |= v;
+    }
+    return re;
+}
+
 //roll 28bit variable left 1 or 2
 inline static uint32_t rol28(uint32_t data, uint8_t i)
 {
@@ -49,7 +62,7 @@ uint64_t doDES(uint64_t key, uint64_t data)
     uint32_t C[17];
     uint32_t D[17];
     C[0] = __bextr_u64(K[0], 0x1c00);
-    D[0] = __bextr_u64(K[0], 0x1c1c);
+    D[0] = __bextr_u64(K[0], 0x1c1b);
     for (int i = 1; i < 17; i++) {
         C[i] = rol28(C[i - 1], host_SHIFT[i - 1]);
         D[i] = rol28(D[i - 1], host_SHIFT[i - 1]);
@@ -60,4 +73,22 @@ uint64_t doDES(uint64_t key, uint64_t data)
         p |= D[i];
         K[i] = permutate56To48(p, host_PC2);
     }
+    //2
+    uint64_t M = permutate64To64(data, host_IP);
+    uint32_t L[17];
+    uint32_t R[17];
+    L[0] = __bextr_u64(M, 0x2000);
+    R[0] = __bextr_u64(M, 0x201f);
+    for (int i = 1; i < 17; i++) {
+        L[i] = R[i - 1];
+        R[i] = L[i - 1] ^ f(R[i - 1], K[i]);
+    }
+    M = R[16] << 32;
+    M |= L[16];
+    return permutate64To64(M, host_IP_INV);
+}
+
+uint32_t f(uint32_t data, uint64_t key)
+{
+    //TODO
 }
--- cpu_algorithm.h
+++ cpu_algorithm.h
@@ -4,3 +4,4 @@
 #include <x86intrin.h> //I assume that we are on x86 processor with gcc
 
 uint64_t doDES(uint64_t key, uint64_t data);
+uint32_t f(uint32_t data, uint64_t key);
